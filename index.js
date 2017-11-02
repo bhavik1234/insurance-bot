dfc2476e0a19cc69bca1c8bb58ab9355f24161d7
bhavik1234 <bhavikchandora1234@gmail.com>
2017-11-02 16:06:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,29 +11,6 @@ restService.use(bodyParser.json());
 restService.set("view options", { layout: false });
 restService.use(express.static(__dirname + '/public'));
 
-var fields = ['car', 'price', 'color'];
-var myCars = [
-    {
-        "car": "Audi",
-        "price": 40000,
-        "color": "blue"
-    }, {
-        "car": "BMW",
-        "price": 35000,
-        "color": "black"
-    }, {
-        "car": "Porsche",
-        "price": 60000,
-        "color": "green"
-    }
-];
-var csv = json2csv({ data: myCars, fields: fields });
-
-fs.writeFile('public/file.csv', csv, function (err) {
-    if (err) throw err;
-    console.log('file saved');
-});
-
 
 restService.get('/download', function (req, res) {
     res.sendfile(__dirname + '/public/file.csv');
